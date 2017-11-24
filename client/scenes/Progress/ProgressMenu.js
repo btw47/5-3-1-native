8ed527e9de49f0aaf292027346e37ec225e928e4
Bradley Wong <btw47@nau.edu>
2017-11-24 15:24:38 -0700

    detailed progress page working

--- client/scenes/Progress/ProgressMenu.js
+++ client/scenes/Progress/ProgressMenu.js
@@ -2,34 +2,17 @@ import React, { Component, StyleSheet, View } from 'react-native';
 import ActionButton from 'react-native-circular-action-menu';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-class App extends Component {
+class ProgressMenu extends Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
         {/*Rest of App come ABOVE the action button component!*/}
-        <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item
-            buttonColor="#9b59b6"
-            title="New Task"
-            onPress={() => console.log('notes tapped!')}>
-            <Icon name="android-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item
-            buttonColor="#3498db"
-            title="Notifications"
-            onPress={() => console.log('notes tapped!')}>
-            <Icon
-              name="android-notifications-none"
-              style={styles.actionButtonIcon}
-            />
-          </ActionButton.Item>
-          <ActionButton.Item
-            buttonColor="#1abc9c"
-            title="All Tasks"
-            onPress={() => console.log('notes tapped!')}>
-            <Icon name="android-done-all" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
+        <ActionButton
+          buttonColor="rgba(231,76,60,1)"
+          onPress={() => {
+            console.log('hi');
+          }}
+        />
       </View>
     );
   }
